bfe4c8e3898ac96ec898bfceb96f0346fb2282fb
patrick-hu7 <patrickhuuu7@gmail.com>
2023-01-10 14:14:57 -0500

    Add Overworld

---
+++ HolyInsurrectionAFootIntoHeaven/Overworld.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Overworld extends World
+{
+    public Overworld() {    
+        super(900, 600, 1); 
+        
+    }
+}

c3a09cb735b47747905a35b204907443850f042a
340802040 <340802040@gapps.yrdsb.ca>
2023-01-13 02:46:58 -0500

    Add Title

--- StartMenu.java
+++ StartMenu.java
@@ -15,10 +15,24 @@ public class StartMenu extends World
      */
     public StartMenu()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1); 
         
+        setBackground("images/TitleScreen.png");
 
+        // Initialize variables
+        Image playButton = new Image("images/Buttons/PlayButton.png");
+        Image selector = new Image("images/TitleSelector.png");
+        
+        // Add Buttons
+        addObject(playButton, 100, 300);
+        
+        // Add Selector
+        addObject(selector, 100, 300);
+        
+    }
+    
+    public void act() {
         
     }
 }

200ebef06c43402c6942f22f19d3ed3a61fc3d10
340802040 <340802040@gapps.yrdsb.ca>
2023-01-14 02:26:54 -0500

    Update Titlescreen

--- StartMenu.java
+++ StartMenu.java
@@ -11,8 +11,14 @@ public class StartMenu extends World
     // Initialize variables
     Image playButton = new Image("images/Buttons/PlayButton.png");
     Image selector = new Image("images/TitleSelector.png");
+    StartSwordShineEffect shine = new StartSwordShineEffect();
     private boolean added = false;
+    private boolean clicked = false;
+    private SimpleTimer animationTimer = new SimpleTimer();
+    protected GreenfootImage[] titleFrames = new GreenfootImage[5];
+    private int imageIndex = 0;
     
+
     /**
      * Constructor for objects of class StartMenu.
      * 
@@ -20,17 +26,23 @@ public class StartMenu extends World
     public StartMenu() {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1); 
-        
+
+        for(int i = 0; i < 5; i++) {
+            titleFrames[i] = new GreenfootImage("images/Animations/TitleScreenAnimations/TitleScreen0" + i + ".png");
+        }
+
         setBackground("images/TitleScreen.png");
 
         // Add Buttons
         addObject(playButton, 100, 300);
     }
-    
+
     public void act() {
         checkHovering();
+        checkClick();
+        animateTitleScreen();
     }
-    
+
     public void checkHovering() {
         if(Greenfoot.mouseMoved(playButton) && !added) {
             addObject(selector, 100, 300);
@@ -40,5 +52,22 @@ public class StartMenu extends World
             removeObject(selector);
             added = false;
         }
+    }
+
+    public void checkClick() {
+        if(Greenfoot.mouseClicked(selector) && !clicked) {
+            clicked = true;
+            addObject(shine, 600, 400);
+        }
+    }
+
+    public void animateTitleScreen() {
+        if(animationTimer.millisElapsed() < 120) {
+            return;
+        }
+        animationTimer.mark();
+
+        setBackground(titleFrames[imageIndex]);
+        imageIndex = (imageIndex + 1) % titleFrames.length;
     }
 }
---
+++ StartSwordShineEffect.java
@@ -0,0 +1,47 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StartSwordShineEffect here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartSwordShineEffect extends Actor
+{
+    private SimpleTimer animationTimer = new SimpleTimer();
+    private SimpleTimer timer = new SimpleTimer();
+    protected GreenfootImage[] shineFrames = new GreenfootImage[5];
+    private boolean marked = false;
+    private int imageIndex = 0;
+    World cutscene = new Cutscene();
+    
+    public StartSwordShineEffect() {
+        for(int i = 0; i < 5; i++) {
+            shineFrames[i] = new GreenfootImage("images/Animations/ShineAnimations/Shine0" + i + ".png");
+        }
+        
+        setImage("images/Animations/ShineAnimations/Shine00.png");
+    }
+    
+    public void act()
+    {
+        if(!marked) {
+            timer.mark();
+            marked = true;
+        }
+        animateShine();
+        if(timer.millisElapsed() > 1250) {
+            Greenfoot.setWorld(cutscene);
+        }
+    }
+    
+    public void animateShine() {
+        if(animationTimer.millisElapsed() < 250) {
+            return;
+        }
+        animationTimer.mark();
+
+        setImage(shineFrames[imageIndex]);
+        imageIndex = (imageIndex + 1) % shineFrames.length;
+    }
+}

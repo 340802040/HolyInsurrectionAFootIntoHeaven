8959d006fe54d18ca2bc1e42b8153ea7f9b3d2ba
340802040 <340802040@gapps.yrdsb.ca>
2023-01-25 03:15:25 -0500

    Add comments

--- Image.java
+++ Image.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * A class simply to show an image.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Patrick Hu
+ * @version Jan 2023
  */
 public class Image extends Actor
 {

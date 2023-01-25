26ce6f0fad4263fdc66fe1e2e91f13a7ce3c36d2
340802040 <340802040@gapps.yrdsb.ca>
2023-01-24 22:02:38 -0500

    Add chapter 7 enemies

--- Chapter7.java
+++ Chapter7.java
@@ -8,11 +8,45 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Chapter7 extends BattleWorld
 {
+    private EnemyFootSoldier e1 = new EnemyFootSoldier(false);
+    private EnemyFootSoldier e2 = new EnemyFootSoldier(false);
+    private EnemyFootSoldier e3 = new EnemyFootSoldier(false);
+    private EnemyArcher e4 = new EnemyArcher(false);
+    private EnemyArcher e5 = new EnemyArcher(false);
+    private EnemyArcher e6 = new EnemyArcher(false);
+    private EnemyCavalry e7 = new EnemyCavalry(false);
+    private EnemyCavalry e8 = new EnemyCavalry(false);
+    private EnemyCavalry e9 = new EnemyCavalry(false);
+    private EnemyCavalry e10 = new EnemyCavalry(false);
+    private EnemyWizard e11 = new EnemyWizard(false);
+    private EnemyWizard e12 = new EnemyWizard(false);
+    private EnemyWizard e13 = new EnemyWizard(false);
+    private EnemyWizard e14 = new EnemyWizard(false);
+    private EnemyCrusader boss1 = new EnemyCrusader(true);
+    private EnemyCrusader boss2 = new EnemyCrusader(true);
+    
     public Chapter7() {
         super(1200, 800, 1);
+        allies = Ally.getClones(ALLIES);
+        enemies.add(e1);
+        enemies.add(e2);
+        enemies.add(e3);
+        enemies.add(e4);
+        enemies.add(e5);
+        enemies.add(e6);
+        enemies.add(e7);
+        enemies.add(e8);
+        enemies.add(e9);
+        enemies.add(e10);
+        enemies.add(e11);
+        enemies.add(e12);
+        enemies.add(e13);
+        enemies.add(e14);
+        enemies.add(boss1);
+        enemies.add(boss2);
         
         map = new int[][] {
-            {25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25},
+            {25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25},
             {25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25},
             {25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25},
             {25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25, 26, 25, 25, 25, 25, 25, 25, 25, 25},
@@ -40,10 +74,80 @@ public class Chapter7 extends BattleWorld
     }
     
     public void spawn() {
+        // ALLIES
+        for (Ally a : allies) {
+            while (true) {
+                int r = getRandomNumberInRange(0, 5);
+                int c = getRandomNumberInRange(0, 16);
+                if (tileAvailable(r, c)) {
+                    addObject(a, GameWorld.getX(c), GameWorld.getY(r));
+                    map[r][c] = 1;
+                    break;
+                }
+            }
+        }
+        // ENEMIES
+        for (Enemy e : enemies) {
+            if (e.isBoss) continue;
+            while (true) {
+                int r = getRandomNumberInRange(0, GameWorld.GRID_HEIGHT);
+                int c = getRandomNumberInRange(8, GameWorld.GRID_WIDTH - 3);
+                if (tileAvailable(r, c)) {
+                    addObject(e, GameWorld.getX(c), GameWorld.getY(r));
+                    map[r][c] = 2;
+                    break;
+                }
+            }
+        }
         
+        // boss
+        int r = GameWorld.GRID_HEIGHT / 2;
+        int c = GameWorld.GRID_WIDTH - 2;
+        addObject(boss1, GameWorld.getX(c), GameWorld.getY(r));
+        
+        // boss 2
+        r = 8;
+        c = 11;
+        addObject(boss2, GameWorld.getX(c), GameWorld.getY(r));
     }
     
     public void setupStats() {
-        
+        for (Enemy e : enemies) {
+            if (e instanceof EnemyFootSoldier) {
+                e.maxHealth = e.health = 32;
+                e.atk = 9;
+                e.def = 5;
+                e.ev = 2;
+            }
+            if (e instanceof EnemyArcher) {
+                e.maxHealth = e.health = 24;
+                e.atk = 9;
+                e.ev = 4;
+                e.spd = 4;
+            }
+            if (e instanceof EnemyWizard) {
+                e.maxHealth = e.health = 22;
+                e.atk = 9;
+                e.ev = 4;
+                e.spd = 3;
+            }
+            if (e instanceof EnemyCavalry) {
+                e.maxHealth = e.health = 32;
+                e.atk = 8;
+                e.ev = 3;
+                e.spd = 5;
+            }
+            if (e.isBoss) {
+                boss1.name = "The Being";
+                e.maxHealth = e.health = 66;
+                e.atk = 14;
+                e.def = 10;
+                e.ev = 4;
+                e.spd = 8;
+            }
+            if (e.isBoss) {
+                
+            }
+        }
     }
 }
